8dcb8f7eaf48a22df897edcf2a3a4cde4265ffce
Felix Englund <felix.englund@studerande.plushogskolan.se>
2021-03-10 12:27:14 +0100

    added more

---
+++ Source/TrainEngine/TrainAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    class TrainAPI
+    {
+        Train train1 = new Train("Name of train");
+        Station station1 = new Station("Gothenburg");
+        Station station2 = new Station("Stockholm");
+
+        ITravelPlan travelPlan = new TrainPlaner(train1, station1)
+                .HeadTowards(station2)
+                .StartTrainAt("10:23")
+                .StopTrainAt(station2, "14:53")
+            .GeneratePlan();
+    }
+}
---
+++ Source/TrainEngine/TrainPlaner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    class TrainPlaner
+    {
+        public TrainPlaner(Train train, Station station1)
+        { 
+
+        
+        }
+    }
+}
