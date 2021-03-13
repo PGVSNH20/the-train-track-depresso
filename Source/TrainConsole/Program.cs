e7c4d4d3dfa4750f32b0a4e2e29464a822e0a276
Frej Björnsson <frej_bjornsson@hotmail.com>
2021-03-13 16:20:15 +0100

    [Add] relative search path to trains.txt in program.cs

--- Source/TrainConsole/Program.cs
+++ Source/TrainConsole/Program.cs
@@ -5,59 +5,59 @@ using TrainEngine;
 
 namespace TrainConsole
 {
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            Console.WriteLine("Train track!");
+    //            Step 1:
+    //             Parse the traintrack(Data / traintrack.txt) using ORM (see suggested code)
+    //             Parse the trains(Data / trains.txt)
+
+    //             Step 2:
+    //             Make the trains run in treads
+
+    //            Train train1 = new Train("Hogwarts Express");
+    //            Station station1 = new Station("Westeros");
+    //            Station station2 = new Station("Liverpool");
+
+    //            ITravelPlan travelPlan = new TravelPlan(train1)
+    //                    .DepartureAt(station1, "")
+    //                    .ArrivalAt(station2, "10:23")
+    //                .GeneratePlan();
+
+    //            Console.WriteLine();
+    //        }
+    //    }
+    //}
+
+
+
+
+
+    // !!SKRIVER UT TÅGINFO!!
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Train track!");
-            // Step 1:
-            // Parse the traintrack (Data/traintrack.txt) using ORM (see suggested code)
-            // Parse the trains (Data/trains.txt)
 
-            // Step 2:
-            // Make the trains run in treads
 
-            Train train1 = new Train("Hogwarts Express");
-            Station station1 = new Station("Westeros");
-            Station station2 = new Station("Liverpool");
+            List<Train> trains = new List<Train>();
+            string pathToTxt = AppDomain.CurrentDomain.BaseDirectory + "\\Data\\trains.txt";
+            foreach (string row in File.ReadAllLines(pathToTxt))
+            {
+                string[] column = row.Split(',');
+                Train train = new Train();
+                train.Id = Int32.Parse(column[0]);
+                train.Name = column[1];
+                train.MaxSpeed = Int32.Parse(column[2]);
+                train.Operated = Boolean.Parse(column[3]);
+                trains.Add(train);
 
-            ITravelPlan travelPlan = new TravelPlan(train1)
-                    .DepartureAt(station1, "")
-                    .ArrivalAt(station2, "10:23")
-                .GeneratePlan();
+                Console.WriteLine("\nTrain ID: " + train.Id + "\nTrain name: " + train.Name + "\nTrain max speed: " + train.MaxSpeed + "\nTrain is operated: " + train.Operated);
+            }
 
-            Console.WriteLine();
+
         }
     }
 }
-
-
-
-
-
-// !!SKRIVER UT TÅGINFO!!
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-
-
-//            List<Train> trains = new List<Train>();
-
-//            foreach (string row in File.ReadAllLines("C:\\Users\\frejb\\source\\repos\\the-train-track-depresso\\Source\\Data\\trains.txt"))
-//            {
-//                string[] column = row.Split(',');
-//                Train train = new Train();
-//                train.Id = Int32.Parse(column[0]);
-//                train.Name = column[1];
-//                train.MaxSpeed = Int32.Parse(column[2]);
-//                train.Operated = Boolean.Parse(column[3]);
-//                trains.Add(train);
-
-//                Console.WriteLine("\nTrain ID: " + train.Id + "\nTrain name: " + train.Name + "\nTrain max speed: " + train.MaxSpeed + "\nTrain is operated: " + train.Operated);
-//            }
-
-
-//        }
-//    }
-//}
