2af0a04660904eefd20d7924fed5d24bfaac2223
Felix Englund <felix.englund@studerande.plushogskolan.se>
2021-03-16 11:51:01 +0100

    added TrainFileORM

---
+++ Source/TrainEngine/TrainFileORM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    public class TrainFileORM : ITrainFileOrm
+    {
+        private string trainFile;
+
+        public TrainFileORM(string trainFile)
+        {
+            this.trainFile = trainFile;
+        }
+
+        public List<Train> Trains { get; private set; }
+
+        public void Load()
+        {
+            var stationLines = System.IO.File.ReadAllLines(trainFile);
+            Console.WriteLine($"Reading file {trainFile}");
+            Trains = new List<Train>();
+
+            foreach (var stationLine in stationLines)
+            {
+                var stationParts = stationLine.Split(new char[] { ';' });
+
+                if (stationParts == null || stationParts.Length != 2)
+                {
+                    throw new Exception("Wrong data format");
+                }
+
+                Trains.Add(new Train()
+                {
+                    Name = stationParts[0],
+                    MaxSpeed = int.Parse(stationParts[1])
+                });
+            }
+        }
+    }
+}
+
